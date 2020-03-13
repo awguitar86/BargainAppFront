f9ea5a049768aa0a899d7c431389e1068fa818a0
Austin Wright <awguitar86@gmail.com>
2020-03-12 22:20:38 -0600

    adjusted data showing on car and item cards. Styled cards a little bit.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -21,11 +21,12 @@ function Home() {
   const displayCarData = !carData
     ? null
     : carData.map(car => {
-        const carIndex = `${car._id[-5]}${car._id[-4]}${car._id[-3]}${
-          car._id[-2]
-        }${car._id[-1]}`;
+        const carIndex =
+          Math.floor(Math.random() * 100 + 1) *
+          Math.floor(Math.random() * 100 + 1) *
+          car.price;
         return (
-          <div key={carIndex}>
+          <div key={carIndex} className="car">
             <div className="image-wrap">
               <img
                 src={`http://178.128.180.91:8080/${car.imageUrls[0]}`}
@@ -35,23 +36,6 @@ function Home() {
             </div>
             <ul>
               <li>{car.title}</li>
-              <li>{car.description}</li>
-              <li>{car.year}</li>
-              <li>{car.make}</li>
-              <li>{car.model}</li>
-              <li>{car.bodyType}</li>
-              <li>{car.mileage}</li>
-              <li>{car.vin}</li>
-              <li>{car.titleType}</li>
-              <li>{car.color}</li>
-              <li>{car.transmission}</li>
-              <li>{car.cylinders}</li>
-              <li>{car.fuelType}</li>
-              <li>{car.doorCount}</li>
-              <li>{car.condition}</li>
-              <li>{car.driveType}</li>
-              <li>{car.price}</li>
-              <li>{car.isFirmOnPrice ? 'Firm on Price' : 'Open to offers'}</li>
               <li>{car.location}</li>
               <li>{car.sellerType}</li>
             </ul>
@@ -62,11 +46,12 @@ function Home() {
   const displayItemData = !itemData
     ? null
     : itemData.map(item => {
-        const itemIndex = `${item._id[-5]}${item._id[-4]}${item._id[-3]}${
-          item._id[-2]
-        }${item._id[-1]}`;
+        const itemIndex =
+          Math.floor(Math.random() * 100 + 1) *
+          Math.floor(Math.random() * 100 + 1) *
+          item.price;
         return (
-          <div key={itemIndex}>
+          <div key={itemIndex} className="item">
             <div className="image-wrap">
               <img
                 src={`http://178.128.180.91:8080/${item.imageUrls[0]}`}
@@ -76,9 +61,6 @@ function Home() {
             </div>
             <ul>
               <li>{item.title}</li>
-              <li>{item.description}</li>
-              <li>{item.condition}</li>
-              <li>{item.category}</li>
               <li>{item.price}</li>
               <li>{item.location}</li>
             </ul>
@@ -89,9 +71,9 @@ function Home() {
   return (
     <div className="home-wrap">
       <h2>Cars</h2>
-      {displayCarData}
+      <div className="car-wrap">{displayCarData}</div>
       <h2>Items</h2>
-      {displayItemData}
+      <div className="item-wrap">{displayItemData}</div>
     </div>
   );
 }
