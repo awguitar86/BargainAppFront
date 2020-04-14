6ba933e52257e12a2c917cc012c4e4dc064d0d29
Austin Wright <awguitar86@gmail.com>
2020-04-13 22:20:11 -0600

    did some styling. Tried to connect to graphql apollo server

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,72 +8,80 @@ function Home() {
   const [itemData, setItemData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://178.128.180.91:8080/cars').then(res => {
+    const graphqlQuery = {
+      query: `
+        Items {
+          id
+          title
+          category
+          condition
+          description
+          price
+          isFirmOnPrice
+          location
+          imageUrl
+        }
+      `,
+    };
+    axios.get('http://178.128.180.91:8080/cars').then((res) => {
       console.log(res.data.cars);
       setCarData(res.data.cars);
     });
-    axios.get('http://178.128.180.91:8080/items').then(res => {
-      console.log(res.data.items);
-      setItemData(res.data.items);
-    });
+    fetch('http://localhost:4000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(graphqlQuery),
+    })
+      .then((resData) => {
+        if (resData.errors) {
+          throw new Error('Fetching status failed!');
+        }
+        // setItemData(resData);
+        console.log(resData);
+      })
+      .catch((err) => {
+        throw err;
+      });
   }, []);
 
   const displayCarData = !carData
     ? null
-    : carData.map(car => {
+    : carData.map((car) => {
         const carIndex =
           Math.floor(Math.random() * 100 + 1) *
           Math.floor(Math.random() * 100 + 1) *
           car.price;
         return (
-          <div key={carIndex} className="car">
-            <div className="image-wrap">
+          <div key={carIndex} className="home-car">
+            <div className="home-image-wrap">
               <img
                 src={`http://178.128.180.91:8080/${car.imageUrls[0]}`}
                 alt="car"
-                style={{ display: 'block', width: '200px', height: '100%' }}
+                className="home-car-img"
               />
             </div>
-            <ul>
-              <li>{car.title}</li>
-              <li>{car.location}</li>
-              <li>${car.price}</li>
+            <ul className="home-car-info">
+              <li className="home-car-info-item car-title">{car.title}</li>
+              <li className="home-car-info-item car-price">${car.price}</li>
+              <li className="home-car-info-item car-mileage">
+                Mileage: {car.mileage}
+              </li>
+              <li className="home-car-info-item car-location">
+                {car.location}
+              </li>
             </ul>
           </div>
         );
       });
 
-  const displayItemData = !itemData
-    ? null
-    : itemData.map(item => {
-        const itemIndex =
-          Math.floor(Math.random() * 100 + 1) *
-          Math.floor(Math.random() * 100 + 1) *
-          item.price;
-        return (
-          <div key={itemIndex} className="item">
-            <div className="image-wrap">
-              <img
-                src={`http://178.128.180.91:8080/${item.imageUrls[0]}`}
-                alt="Item"
-                style={{ display: 'block', width: '200px', height: '100%' }}
-              />
-            </div>
-            <ul>
-              <li>{item.title}</li>
-              <li>{item.location}</li>
-              <li>${item.price}</li>
-            </ul>
-          </div>
-        );
-      });
+  // const displayItemData
 
   return (
     <div className="home-wrap">
       <h2>Cars</h2>
-      <div className="car-wrap">{displayCarData}</div>
-      <h2>Items</h2>
-      <div className="item-wrap">{displayItemData}</div>
+      <div className="home-car-wrap">{displayCarData}</div>
     </div>
   );
 }
