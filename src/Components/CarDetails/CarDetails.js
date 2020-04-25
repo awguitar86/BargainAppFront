1676a32a05da905b4ef77a45363a16037ba1a368
Austin Wright <awguitar86@gmail.com>
2020-04-24 23:04:49 -0600

    styled the car details page

--- src/Components/CarDetails/CarDetails.js
+++ src/Components/CarDetails/CarDetails.js
@@ -1,20 +1,144 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './carDetails.scss';
+import axios from 'axios';
 
-function CarDetails() {
-  // return (
-  //   <div className="item-details-wrap" style={{ color: 'white' }}>
-  //     <img src={`http://localhost:8080/${imageUrl}`} alt="car" />
-  //     <h2>{title}</h2>
-  //     <h3>Price: ${price}</h3>
-  //     <h3>Description: {description}</h3>
-  //     <h3>Category: {category}</h3>
-  //     <h3>Condition: {condition}</h3>
-  //     <h3>Location: {location}</h3>
-  //     <h3>Firm on Price: {isFirmOnPrice ? 'Yes' : 'No'}</h3>
-  //   </div>
-  // );
-  return <h1>Car Details</h1>;
+function CarDetails(props) {
+  const [carData, setCarData] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/cars/${props.match.params.id}`)
+      .then((res) => {
+        console.log(res);
+        setCarData(res.data.car);
+      });
+  }, [props.match.params.id]);
+
+  console.log(carData);
+
+  // const displayImages = !imageUrls
+  //   ? null
+  //   : imageUrls.map((image) => {
+  //       return (
+  //         <img src={`http://localhost:8080/${image}`} alt="car" key={} />
+  //       );
+  //     });
+
+  if (carData) {
+    const {
+      _id,
+      bodyType,
+      color,
+      condition,
+      cylinders,
+      description,
+      doorCount,
+      driveType,
+      fuelType,
+      isFirmOnPrice,
+      location,
+      make,
+      mileage,
+      model,
+      price,
+      sellerName,
+      sellerPhone,
+      title,
+      titleType,
+      transmission,
+      vin,
+      year,
+      imageUrls,
+    } = carData;
+
+    return (
+      <div className="car-details-wrap">
+        <div className="car-left">
+          <img src={`http://localhost:8080/${imageUrls[0]}`} alt="car" />
+          <h1>{title}</h1>
+          <h2>${price}</h2>
+          <h3>
+            Seller Name:{' '}
+            <span style={{ fontWeight: 'normal' }}>{sellerName}</span>
+          </h3>
+          <h3>
+            Seller Phone:{' '}
+            <span style={{ fontWeight: 'normal' }}>{sellerPhone}</span>
+          </h3>
+          <h3>{location}</h3>
+          <h3>
+            Description:{' '}
+            <span style={{ fontWeight: 'normal' }}>{description}</span>
+          </h3>
+        </div>
+        <div className="car-specs">
+          <h2>Specifications</h2>
+          <div>
+            <h3>Year:</h3>
+            <p>{year}</p>
+          </div>
+          <div>
+            <h3>Make:</h3>
+            <p>{make}</p>
+          </div>
+          <div>
+            <h3>Model:</h3>
+            <p>{model}</p>
+          </div>
+          <div>
+            <h3>Body:</h3>
+            <p>{bodyType}</p>
+          </div>
+          <div>
+            <h3>Mileage:</h3>
+            <p>{mileage}</p>
+          </div>
+          <div>
+            <h3>VIN:</h3>
+            <p>{vin.toUpperCase()}</p>
+          </div>
+          <div>
+            <h3>Title Type:</h3>
+            <p>{titleType}</p>
+          </div>
+          <div>
+            <h3>Color:</h3>
+            <p>{color}</p>
+          </div>
+          <div>
+            <h3>Transmission:</h3>
+            <p>{transmission}</p>
+          </div>
+          <div>
+            <h3>Cylinders:</h3>
+            <p>{cylinders}</p>
+          </div>
+          <div>
+            <h3>Fuel Type:</h3>
+            <p>{fuelType}</p>
+          </div>
+          <div>
+            <h3>Number of Doors:</h3>
+            <p>{doorCount}</p>
+          </div>
+          <div>
+            <h3>Condition:</h3>
+            <p>{condition}</p>
+          </div>
+          <div>
+            <h3>Drive Type:</h3>
+            <p>{driveType}</p>
+          </div>
+          <div>
+            <h3>Firm on Price:</h3>
+            <p>{isFirmOnPrice ? 'Yes' : 'No'}</p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <h3>No Data</h3>;
+  }
 }
 
 export default CarDetails;
